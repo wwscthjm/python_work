33aca03ae7856c0f794f6f734a2c0505eeb479c1
CJ W <wwscthjm@gmail.com>
2018-06-13 19:48:17 +0800

    201806131948

---
+++ name_cases.py
@@ -0,0 +1,18 @@
+#message = "Hello Eric, would you like to learn some Python today?"
+#print(message)
+
+#name = "PY"
+#print(name.title())
+#print(name.upper())
+#print(name.lower())
+
+famous_person = "Albert Einstein"
+#quote = '"A person who never made a mistake never tried anything new."'
+#message = famous_person + " once said, " + quote
+#print(message)
+
+name = " " + famous_person + " "
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
