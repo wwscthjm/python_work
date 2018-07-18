0c3d4af99ad33d842465e3012f0e7aef61e2349b
CJ W <wwscthjm@gmail.com>
2018-07-18 11:20:05 +0800

    201807181119

---
+++ motorcycles.py
@@ -0,0 +1,22 @@
+#motocycles = ['honda', 'yamaha', 'suzuki']
+#print(motocycles)
+
+##motocycles[0] = 'ducati'
+##print(motocycles)
+
+#motocycles.append('ducati')
+#print(motocycles)
+
+motocycles = []
+
+motocycles.append('honda')
+motocycles.append('yamaha')
+motocycles.append('suzuki')
+
+print(motocycles)
+
+#motocycles.insert(0, 'ducati')
+#print(motocycles)
+
+del motocycles[0]                   #The value of motocycles[0] doesn't exist anymore.
+print(motocycles)
