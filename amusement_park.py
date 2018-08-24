7c06be036111d7b84cf37bddcfb899771eb82461
CJ W <wwscthjm@gmail.com>
2018-08-25 00:02:17 +0800

    201808250002

---
+++ amusement_park.py
@@ -0,0 +1,16 @@
+age = 63
+
+if age < 4:
+    #print('Your admission cost is $0.')
+    price = 0
+elif age < 18:
+    #print('Your admission cost is $5.')
+    price = 5
+elif age < 65:
+    #print('Your admission cost is $10.')
+    price = 10
+else:
+    #print('Your admission cost is $5.')
+    price = 5
+
+print('Your admission cost is $' + str(price) + '.')
---
+++ cars.py
@@ -0,0 +1,7 @@
+cars = ['bmw', 'auid', 'toyato', 'subaru']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
