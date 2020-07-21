99efde182ffd9856d181e1875941a3fd27233464
CJ W <wwscthjm@gmail.com>
2020-07-21 20:08:00 +0800

    202007212007

---
+++ Chapter2_Strings/test.py
@@ -0,0 +1,36 @@
+# java:final
+# python: 命名的时候大写，表示常量值
+NAME= "'name'"
+print(NAME)
+print('hello')
+value = 'hello'
+print(value)
+value ='python'
+print(value)
+value = 'python1905班'
+print(value)
+
+#字符串： 字符串 表示：'' "" ,''''''
+messeage = '[淘宝]你正在使用验证码登录，\n验证码是：8906，\n请保密。'
+print(messeage)
+
+messeage1= '''
+[淘宝]你正在使用验证码登录，
+验证码是：8906，
+涉及个人的账户安全，请保密。
+'''
+
+print(messeage1)
+messeage2='''
+[淘宝]你正在使用验证码登录，
+验证码是：8906，
+涉及个人的账户安全，请保密。
+'''
+
+print(messeage2)
+#三引号作用：1.保留格式的字符串使用 2.作为注释使用
+Name = '''admin'''
+Name = "admin"
+Name="'name'"
+Name='''name'''
+print(Name)
--- Chapter9_Class/dog.py
+++ Chapter9_Class/dog.py
@@ -6,7 +6,7 @@ class Dog():
     def __init__(self, name, age):
         """初始化属性name和age"""
         self.name = name
-        self.age = age
+        self.age =  age
 
     def sit(self):
         """模拟小狗被命令时蹲下"""
@@ -17,8 +17,13 @@ class Dog():
         print(self.name.title() + " rolled over.")
 
 my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
 
-#print("My dog's name is " + my_dog.name.title() + ".")
-#print("My dog is " + str(my_dog.age) + " years old.")
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
 my_dog.sit()
-my_dog.roll_over()
+#my_dog.roll_over()
+
+print("Your dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " years old.")
+your_dog.sit()
