38103d698eaf7ba4926b368c7ded7a78596d877d
CJ W <wwscthjm@gmail.com>
2020-08-10 00:04:28 +0800

    20200810 work done

--- Chapter18+19+20/learning_logs/views.py
+++ Chapter18+19+20/learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
 
 
 # Create your views here.
@@ -9,15 +12,31 @@ def index(request):
 
 
 def topics(request):
-    """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
-    return render(request, 'learning_logs/topics.html', context)
+    """Show all topics_p."""
+    topics_p = Topic.objects.order_by('date_added')
+    context = {'topics_p': topics_p}
+    return render(request, 'learning_logs/topics_p.html', context)
 
 
 def topic(request, topic_id):
-    """Show a single topic, and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
+    """Show a single topic_p, and all its entries."""
+    topic_p = Topic.objects.get(id=topic_id)
+    entries = topic_p.entry_set.order_by('-date_added')
+    context = {'topic_p': topic_p, 'entries': entries}
+    return render(request, 'learning_logs/topic_p.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
