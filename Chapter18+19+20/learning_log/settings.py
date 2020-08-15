4542d5bb2273d3f403dee4a023f469fa38204865
CJ W <wwscthjm@gmail.com>
2020-08-15 22:32:38 +0800

    Chapter 20 finished

--- Chapter18+19+20/learning_log/settings.py
+++ Chapter18+19+20/learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd part application
+    'bootstrap3',
 
     # My application.
     'learning_logs',
@@ -125,3 +128,29 @@ STATIC_URL = '/static/'
 
 # My settings.
 LOGIN_URL = '/users/login/'
+
+# bootstrap3 settings
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
+
+# Heroku settings
+cwd = os.getcwd()
+if cwd == '/app' or cwd[:4] == '/tmp':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # Let request.is_secure() acknowledge X-Forwarded-Proto head
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Support all host header
+    ALLOWED_HOSTS = ['*']
+
+    # Static property settings
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
